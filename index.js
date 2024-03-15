0ce3fd29acf066204150d4d3f8ff1d921e26b48f
Berkay Atalay <berkayatalay90@gmail.com>
2024-03-15 15:47:45 -0400

    hide the menu icons

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Email Validation
 
 function validateEmail() {
@@ -6,7 +5,7 @@ function validateEmail() {
     const email = document.getElementById("email").value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const emailWarningWrapper = document.getElementsByClassName("email-warning-wrapper")[0];
-    
+
 
     if (emailRegex.test(email)) {
         emailWarningWrapper.style.maxHeight = null
@@ -16,4 +15,47 @@ function validateEmail() {
         econsole.log("email is not valid");
     }
 }
+
+// Hamburger Menu for Mobile Design
+
+// Hide the font awesome menu icons
+// Close Icon X
+
+var menuIconClose = document.getElementById('menuIconClose');
+menuIconClose.style.display = 'none';
+
+// Open Icon
+var menuIconOpen = document.getElementById("menuIconOpen");
+
+function checkScreenWidth() {
+    var width = window.innerWidth;
+    var minWidth = 480;
+
+    if (width <= minWidth) {
+        menuIconOpen.style.display = 'block';
+    } else {
+        menuIconOpen.style.display = 'none';
+    }
+}
+
+// Check on page load
+window.onload = checkScreenWidth;
+
+// Check when the screen size changes
+window.addEventListener('resize', checkScreenWidth);
+
+
+
+
+// let menuIconOpen = document.getElementById("menuIconOpen");
+// let menuIconClose = document.getElementById("menuIconClose");
+
+
+
+// menuIconOpen.addEventListener("click", function () {
+//     menuIconOpen.classList.add("hidden");
+//     menuIconClose.classList.toggle("hidden");
+// })
+
+
 
